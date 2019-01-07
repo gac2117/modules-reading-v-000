88e70afab7fc54fe9867adfd091dcf94aee2ae58
Grace Choi <oiograceoio@gmail.com>
2019-01-07 04:53:15 +0000

    Automatically backed up by Learn

--- lib/dance_module.rb
+++ lib/dance_module.rb
@@ -1,13 +1,13 @@
-module Dance 
+module Dance
   def twirl
     "I'm twirling!"
   end
 
-  def jump 
+  def jump
     "Look how high I'm jumping!"
   end
 
-  def pirouette 
+  def pirouette
     "I'm doing a pirouette"
   end
 
